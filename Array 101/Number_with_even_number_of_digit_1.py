8afef54223442f0ba144579878558d614ae18785
Daksh Singh <48284318+Daksh-Singh@users.noreply.github.com>
2020-08-02 11:19:18 +0530

    Delete Number_with_even_number_of_digit_1.py

--- Array 101/Number_with_even_number_of_digit_1.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def findNumbers(self, nums: List[int]) -> int:
-        flag=0
-        for i in range(len(nums)):
-            nums[i]=len(str(nums[i]))
-            if nums[i]%2==0:
-                flag+=1
-        return flag
