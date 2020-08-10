2b2999e6cfe97e3c49d4c0bcaadcf7e3c9f2f733
Daksh Singh <48284318+Daksh-Singh@users.noreply.github.com>
2020-08-10 15:03:37 +0530

    Runtime Efficient method

---
+++ Array 101/Valid_Mountain_array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def validMountainArray(self, A: List[int]) -> bool:
+        l=len(A)
+        if l<3:
+            return False
+        else:
+            
+            i=0
+            while(i+1<l and A[i]<A[i+1]):
+                i+=1
+            if i==0 or i==l-1:
+                return False
+            while(i+1<l and A[i]>A[i+1]):
+                i+=1
+            return i==l-1
