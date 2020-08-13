2a57ec723bc2563556ab4831e12e6637150de5ab
Daksh Singh <48284318+Daksh-Singh@users.noreply.github.com>
2020-08-13 15:35:15 +0530

    Memory and Runtime efficient
    Using quick sort technique.

---
+++ Array 101/Sort_Array_by_Parity.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+        l = 0
+        r = len(A)-1
+        while (l < r):
+            if A[l] % 2 != 0:
+                A[l], A[r] = A[r],A[l]
+                r-=1
+            else:
+                l+=1
+        return A
