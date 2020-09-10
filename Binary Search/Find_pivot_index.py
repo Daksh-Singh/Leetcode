53904a8421c76c785ba04f9290865a059e59ad83
Daksh Singh <48284318+Daksh-Singh@users.noreply.github.com>
2020-09-10 18:42:38 +0530

    Delete Find_pivot_index.py

--- Binary Search/Find_pivot_index.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def pivotIndex(self, nums: List[int]) -> int:
-        for i in range(0,len(nums),1):
-            if sum(nums[:i]) == sum(nums[i+1:]):
-                return i
-        return -1 
-        
