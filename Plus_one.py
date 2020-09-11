b79d8268b74d143193bdc4fe5f0d41b9451fe04f
Daksh Singh <48284318+Daksh-Singh@users.noreply.github.com>
2020-09-11 16:35:39 +0530

    Delete Plus_one.py

--- Plus_one.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        st = ""
-        for i in digits:
-            st+=str(i)
-        st = str(int(st)+1)
-        digits = [int(char) for char in st]
-        return digits
