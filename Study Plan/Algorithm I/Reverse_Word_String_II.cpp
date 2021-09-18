26e487e3259167679685970ae94acf08f65311ee
Daksh Singh <48284318+Daksh-Singh@users.noreply.github.com>
2021-09-18 12:46:31 +0530

    Using stack to reverse the words

---
+++ Study Plan/Algorithm I/Reverse_Word_String_II.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stack<char> st;
+        int now = 0;
+        string ret;
+        int n = s.length();
+        int i = 0;
+        while(i < n)
+        {
+            if(isspace(s[i]))
+            {
+                if(now > 0)
+                    ret += ' ';
+                while(!st.empty())
+                {
+                    char c = st.top();
+                    st.pop();
+                    ret += c;
+                }
+                now ++;
+            }
+            if(!isspace(s[i]))
+                st.push(s[i]);
+            i ++;
+        }
+        if(now > 0)
+            ret += ' ';
+        while(!st.empty())
+        {
+            char c = st.top();
+            st.pop();
+            ret += c;
+        }
+        return ret;
+    }
+};
