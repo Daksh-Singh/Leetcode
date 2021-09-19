12f40dcef2cd92f26230655f5170c1e9cb5b8c2f
Daksh Singh <48284318+Daksh-Singh@users.noreply.github.com>
2021-09-19 10:44:56 +0530

    Using counter approach

---
+++ Study Plan/Algorithm I/Remove_Nth_Node.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        curr = head
+        count = head
+        size = 1    
+
+        while count.next is not None:
+            count = count.next
+            size += 1
+
+        if size == 1:
+            head = None
+        elif size == n:
+            head = curr.next
+        else:
+            for i in range(size - n - 1):
+                curr = curr.next
+            curr.next = curr.next.next
+        return head
